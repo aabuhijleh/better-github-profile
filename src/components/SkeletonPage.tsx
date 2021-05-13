78e98b48686fffef2c0f145fb93ae76395ce10d3
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-13 09:02:01 +0300

    feat: lower skeleton animation duration

--- src/components/SkeletonPage.tsx
+++ src/components/SkeletonPage.tsx
@@ -3,16 +3,17 @@ import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 export const SkeletonPage: React.FC = () => {
+  const duration = 0.6;
   return (
     <div style={{ width: "70%" }}>
       <SkeletonTheme color="#202020" highlightColor="#444">
         <h1>
-          <Skeleton width={"30%"} />
+          <Skeleton width={"30%"} duration={duration} />
         </h1>
         <p>
-          <Skeleton width={"80%"} height={"20vh"} />
+          <Skeleton width={"80%"} height={"20vh"} duration={duration} />
         </p>
-        <Skeleton width={"80%"} height={"60vh"} />
+        <Skeleton width={"80%"} height={"60vh"} duration={duration} />
       </SkeletonTheme>
     </div>
   );
