b2b1ba5c2f9bf7e231e9385ddcd0f9d73d31882e
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-14 01:27:51 +0300

    feat: show error if no profile readme is found

--- src/utils/useGithubReadme.ts
+++ src/utils/useGithubReadme.ts
@@ -7,15 +7,23 @@ export const useGithubReadme = (username: string) => {
 
   useEffect(() => {
     const fetchReadme = async () => {
-      const octokit = new Octokit();
-      const readme = await octokit.rest.repos.getReadme({
-        owner: username,
-        repo: username,
-      });
-      setReadme({
-        content: Base64.decode(readme.data.content),
-        loading: false,
-      });
+      try {
+        const octokit = new Octokit();
+        const readme = await octokit.rest.repos.getReadme({
+          owner: username,
+          repo: username,
+        });
+        setReadme({
+          content: Base64.decode(readme.data.content),
+          loading: false,
+        });
+      } catch (error) {
+        console.error("could not fetch readme", error);
+        setReadme({
+          content: `### Could not download your profile README :( \n\n ### [Here's the documentation](https://docs.github.com/en/github/setting-up-and-managing-your-github-profile/managing-your-profile-readme)`,
+          loading: false,
+        });
+      }
     };
     fetchReadme();
   }, [username]);
