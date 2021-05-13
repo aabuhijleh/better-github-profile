a516744bb94c2481ad2c409674aa4ca15a227f27
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-13 12:48:30 +0300

    refactor: rename func to fetchReadme

--- src/utils/useGithubReadme.ts
+++ src/utils/useGithubReadme.ts
@@ -4,14 +4,14 @@ export const useGithubReadme = (username: string) => {
   const [readme, setReadme] = useState({ content: "", loading: true });
 
   useEffect(() => {
-    const fetchResume = async () => {
+    const fetchReadme = async () => {
       const response = await fetch(
         `https://raw.githubusercontent.com/${username}/${username}/main/README.md`
       );
       const readme = await response.text();
       setReadme({ content: readme, loading: false });
     };
-    fetchResume();
+    fetchReadme();
   }, [username]);
 
   return readme;
