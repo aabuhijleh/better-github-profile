4fb1631cf387f0e557203bc496595d79059954dc
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-14 12:40:36 +0300

    feat: do not persist state in localstorage

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,11 @@
 import create from "zustand";
-import { persist } from "zustand/middleware";
 
 interface AppState {
   username: string;
   setUsername: (to: string) => void;
 }
 
-export const useStore = create<AppState>(
-  persist(
-    (set) => ({
-      username: "aabuhijleh",
-      setUsername: (to) => set({ username: to }),
-    }),
-    {
-      name: "user-storage",
-    }
-  )
-);
+export const useStore = create<AppState>((set) => ({
+  username: "aabuhijleh",
+  setUsername: (to) => set({ username: to }),
+}));
