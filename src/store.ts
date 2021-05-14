5c2949304ab711719a4e6de281db8fa80c336aa0
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-14 17:16:41 +0300

    feat: support setting username via URL params

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,17 @@
 import create from "zustand";
+import queryString from "query-string";
+import { defaults } from "src/defaults";
 
 interface AppState {
   username: string;
   setUsername: (to: string) => void;
 }
 
+const username = queryString.parse(document.location.search)[
+  "username"
+] as string;
+
 export const useStore = create<AppState>((set) => ({
-  username: "aabuhijleh",
+  username: username || defaults.username,
   setUsername: (to) => set({ username: to }),
 }));
