8a2a7bb0b8dd8d6247257361594d410b8c6c7d6c
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-15 08:47:23 +0300

    refactor: folder structure

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div>© Abdurrahman Abu-Hijleh 2021</div>
+      <div>
+        Created with{" "}
+        <a href="https://github.com/aabuhijleh/github-personal-website-generator">
+          GitHub Personal Website Generator
+        </a>
+      </div>
+    </footer>
+  );
+};
