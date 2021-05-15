9a12745685c61eb4a6015fd1ee9f38c9290faaf4
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-15 13:16:02 +0300

    refactor: remove unused resume route

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,12 @@ import { Footer } from "src/components/Footer";
 import { ChangeUsernameButton } from "src/components/ChangeUsernameButton";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Landing } from "src/components/Pages/Landing";
-import { Resume } from "src/components/Pages/Resume";
 import "src/styles/app.scss";
 
 export const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/resume">
-          <Resume />
-        </Route>
         <Route path="/">
           <Landing />
         </Route>
--- src/components/ResumeLayout.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ResumeLayout: React.FC = ({ children }) => {
-  return <div className="resume-layout">{children}</div>;
-};
