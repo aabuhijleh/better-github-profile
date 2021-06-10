351ab951203b6368b13fc9418697e21c2ac83502
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-06-10 19:36:40 +0300

    wip: setup apollo client

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { ThemeProvider } from "styled-components/macro";
 import { GlobalStyle } from "src/styles/global";
-import { usePersistentStore } from "src/store";
+import { usePersistentStore, useStore } from "src/store";
+import { useUserQuery } from "src/generated/graphql";
 
 export const App: React.FC = () => {
+  const username = useStore((state) => state.username);
   const mode = usePersistentStore((state) => state.mode);
+
+  const test = useUserQuery({ variables: { username } });
+
+  console.log("testinnnnng", test);
 
   return (
     <ThemeProvider theme={{ mode }}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "src/components/App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
