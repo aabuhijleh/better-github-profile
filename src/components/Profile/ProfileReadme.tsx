1babc0ed57ab76e3602cb199ae063a7eb9f65546
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-06-13 19:44:35 +0300

    feat: improve readme skeleton

--- src/components/Profile/ProfileReadme.tsx
+++ src/components/Profile/ProfileReadme.tsx
@@ -88,13 +88,6 @@ export const ProfileReadme: React.FC = () => {
           <rect x="75%" y="505" rx="5" ry="5" width="30%" height="8" />
           <rect x="0" y="525" rx="5" ry="5" width="65%" height="8" />
           <rect x="75%" y="525" rx="5" ry="5" width="30%" height="8" />
-          <rect x="75%" y="550" rx="5" ry="5" width="10%" height="10" />
-          <circle cx="76.5%" cy="590" r="18" />
-          <circle cx="80%" cy="590" r="18" />
-          <circle cx="83.5%" cy="590" r="18" />
-          <circle cx="87%" cy="590" r="18" />
-          <circle cx="90.5%" cy="590" r="18" />
-          <circle cx="94%" cy="590" r="18" />
         </ContentLoader>
       </Wrapper>
     );
