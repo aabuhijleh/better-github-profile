2167c03d29a9431d0358ce0ca9f01b12f7ab5697
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-06-13 18:41:38 +0300

    refactor: change default values

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -3,6 +3,6 @@ import { DefaultValues } from "src/types";
 export const defaults: DefaultValues = {
   username: "aabuhijleh",
   mode: "dark",
-  soundEnabled: false,
-  documentTitle: "MyGitHubProfile",
+  soundEnabled: true,
+  documentTitle: "",
 };
